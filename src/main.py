d8e02727ebc160a69e00a0caf019610d3a0e7844
GregPolack <grzelock@gmail.com>
2024-07-16 14:45:21 +0200

    add main.py and TextNode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+from textnode import TextNode
+
+def main():
+
+    node = TextNode("This is a text node", "bold", "boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
