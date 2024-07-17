30ec7f2a979f5d64ed237e8a140acdd47dc61bf1
GregPolack <grzelock@gmail.com>
2024-07-17 21:06:07 +0200

    reorganize LeafNode into htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -15,5 +15,19 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "This is some text", None, {"href": "https://www.google.com", "target": 6})
         self.assertNotEqual(node.props_to_html(), 'href="https://www.google.com" target="_blank"' )
 
+if __name__ == "__main__":
+    unittest.main()
+
+class TestLeafNode(unittest.TestCase):
+    def test_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        result = '<p>This is a paragraph of text.</p>'
+        self.assertEqual(node.to_html(), result)
+   
+    def test_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com", "target": "_blank"})
+        result = '<a href="https://www.google.com" target="_blank">Click me!</a>'
+        self.assertEqual(node.to_html(), result)
+        
 if __name__ == "__main__":
     unittest.main()
