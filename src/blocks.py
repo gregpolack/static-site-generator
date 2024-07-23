1531e2b3cb99a086e833833c9b65f10d22429c12
GregPolack <grzelock@gmail.com>
2024-07-23 14:19:26 +0200

    add block_to_block_type function

--- src/blocks.py
+++ src/blocks.py
@@ -5,3 +5,36 @@ def markdown_to_blocks(markdown):
     filtered_blocks = list(filter(lambda x: x != "", stripped_blocks))
 
     return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    heading_prefixes = ["# ", "## ", "### ", "#### ", "##### ", "###### " ]
+
+    if block.startswith(tuple(heading_prefixes)):
+        return "heading"
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return "code"
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return "paragraph"
+        return "quote"
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return "paragraph"
+        return "unordered_list"
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return "paragraph"
+        return "unordered_list"
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return "paragraph"
+            i += 1
+        return "ordered_list"
+    return "paragraph"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -21,5 +21,62 @@ class TestMarkdownToBlocks(unittest.TestCase):
 
         self.assertListEqual(expected_result, result)
 
+class TestBlockToBlockType(unittest.TestCase):
+    def normal_paragraph(self):
+        input = "This is a start of a paragraph.\nIt continues here."
+        expected_result = "paragraph"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+
+    def test_heading(self):
+        input = "# This is a heading"
+        expected_result = "heading"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+    
+    def test_multiple_headings(self):
+        input = "#### This is a different heading"
+        expected_result = "heading"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+    
+    def test_code(self):
+        input = "```\nThis is a code block\n```"
+        expected_result = "code"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+
+    def test_quote(self):
+        input = "> This is a quote block\n>It continues to the next line here."
+        expected_result = "quote"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+    
+    def test_unordered_list_asterisk(self):
+        input = "* This is a start of an unordered list\n* It continues to the next line."
+        expected_result = "unordered_list"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+    
+    def test_unordered_list_dash(self):
+        input = "- This is a start of an unordered list\n- It continues to the next line."
+        expected_result = "unordered_list"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+
+    def test_ordered_list(self):
+        input = "1. This is a start of an ordered list\n2. It continues to the next line."
+        expected_result = "ordered_list"
+        result = block_to_block_type(input)
+
+        self.assertEqual(expected_result, result)
+
 if __name__ == "__main__":
     unittest.main()
